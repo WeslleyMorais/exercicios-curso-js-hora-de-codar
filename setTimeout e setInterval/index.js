7fdcdd5d5676fdd72e353d61bfb84a83d6bf10d1
WeslleyMorais <166230826+WeslleyMorais@users.noreply.github.com>
2024-07-17 22:07:53 -0300

    A46 setTimeout

---
+++ setTimeout e setInterval/index.js
@@ -0,0 +1,12 @@
+//setTimeout - Executam depois de um tempo
+console.log('Antes do setTimeout');
+
+setTimeout(function(){
+    console.log('Teste do setTimeout');
+}, 5000);
+
+
+//setInterval - Executam depois de tempo em tempo
+setInterval(function(){
+    console.log('Testando setInetrval');
+}, 2000);
